da3bc3212e1d67b016d68e099674772e8f4b5ea0
Kevin <knphillips01@gmail.com>
2020-02-09 16:23:55 -0600

    finished response material project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
+import Grid from '@material-ui/core/Grid';
 import './App.css';
-
+import PplCard from './PplCard'; 
 
 function App() {
   const[users, setUsers] = useState([]);
@@ -8,9 +9,10 @@ function App() {
   useEffect(()=>{
     async function fetchData() {
       setUsers(
-        fetch('https://reqres.in/api/users?page=2')
-        .then(res=>res.json())
-        .then(res=> console.log(res))
+        await fetch('https://reqres.in/api/users?page=2')
+        .then(res => res.json())
+        .then(res => res.data)
+        .catch(err => console.log(err))
       )
     }
     fetchData();
@@ -20,6 +22,27 @@ function App() {
   return (
     <div className="App">
       <h3>Responsive Material UI</h3>
+      <Grid
+        container
+        spacing={10}
+        style={{padding: '24px'}}
+        >
+       {users.map( users => 
+        <Grid
+          key={users.id}
+          item
+          xs={12} sm={6} md={4} lg={4} xl={3}
+        >
+          <PplCard
+          key={users.id} 
+          email={users.email}
+          firstname={users.first_name}
+          lastname={users.last_name}
+          avatar={users.avatar}
+          />
+        </Grid>
+        )}
+      </Grid>
     </div>
   );
 }
---
+++ src/PplCard.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+});
+
+export default function PplCard({email, firstname, lastname, avatar}) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          alt="Contemplative Reptile"
+          height="140"
+          image={avatar}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {firstname + " " + lastname}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {email}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button href={avatar} size="small" color="primary">
+          Thumbnail Pic
+        </Button>
+      </CardActions>
+      </Card>
+  );
+}
